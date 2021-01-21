214dd8877ee865ee9aea234ee9e1d79944af44c7
skbrain <gitskbrain@bk.ru>
2021-01-22 02:16:01 +0300

    task 1 done task 2 done task 3 done task 4 done task 5 done

---
+++ Task_4.py
@@ -0,0 +1,9 @@
+num = int(input("Введите любое число: "))
+
+max_num = num % 10
+num = num // 10
+while num > 0:
+    if num % 10 > max_num:
+        max_num = num % 10
+    num = num // 10
+print(max_num)
