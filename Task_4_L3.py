29d84460ef01e13480c2292172c1b823b62278a3
skbrain <gitskbrain@bk.ru>
2021-01-29 16:49:43 +0300

    Task_6_L3 done

--- Task_4_L3.py
+++ Task_4_L3.py
@@ -1,7 +1,10 @@
 x = float(input("Введите действительное положительное число x: "))
 y = int(input("Введите целое отрицательное число y: "))
-def my_func(x,y):
-    result = (x**1/(abs(y)))
+
+
+def my_func(x, y):
+    result = (x ** 1 / (abs(y)))
     return result
 
-print(my_func(4,2))
+
+print(my_func(4, 2))
