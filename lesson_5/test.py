ad499eba6ff87fa7b40d9332e656d941260922d6
skbrain <gitskbrain@bk.ru>
2021-02-04 00:05:38 +0300

    Task_4_L5 done

---
+++ lesson_5/test.py
@@ -0,0 +1,10 @@
+eng_rus_dic = {"One": "один", "Two": "Два", "Three": "три", "Four": "четыре"}
+print(eng_rus_dic)
+with open("txttask_4.txt", "r") as f:
+    line = f.readlines()
+    for i in line:
+        if i.split()[0] in eng_rus_dic:
+            for x in rangei.split()
+            print(eng_rus_dic[i.split()[0]] + " " +)
+
+
